9ebf40c944be6b4eb5835a977d803db39fc9a2f7
lingmao <lingmao@lingtutech.com>
2024-09-24 16:17:50 +0800

    test

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ html {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin: 0;
   box-sizing: border-box;
+  margin: 0;
 }
 </style>
